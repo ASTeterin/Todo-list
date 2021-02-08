dfc20d1add3d087fbf4b43a5e45656a9c819c600
ASTeterin <at28813905@gmail.com>
2021-02-08 16:31:35 +0300

    add beta checking request params

--- add.php
+++ add.php
@@ -1,16 +1,41 @@
 <?php
 
 require_once('inc/common.inc.php');
+const COLUMN_NAMES = ['id_task', 'task_name', 'task_text', 'is_done'];
+
+function checkRequestedData($data) {
+    foreach($data as $key => $value) {
+        if (!in_array($key, COLUMN_NAMES)) {
+            echo 'error in column name';
+            return 1;
+        }
+        if (strlen($value) > MAX_TASK_TEXT_LEN) {
+            echo 'max len';
+            return 1;
+        }
+    }
+    return 0;   
+}
 
 $database = new Database();
 $db = $database->getConnection();
 //print_r($db);
 $task = new Task($db);
+$data = [
+    'task_name1' => 'qwerty',
+    'task_text' => 'ehwfhewjkgwkjghjh hjegkhekgh eghrjhj erhejerkehjjehk'
+];
 
-$input = json_decode(
-    file_get_contents('php://input'),
-    true
-);
+if (!checkRequestedData($data)) {
+    $result = $task->add($data);
+    if ($result)
+        echo 'task was added';
+    
+    }
+    else {
+        header('HTTP/1.0 400 Bad Request');
+        echo json_encode(array(
+        'error' => 'Bad Request'
+    ));
+}
 
-//var_dump($input);
-$task->add();
--- inc/task.inc.php
+++ inc/task.inc.php
@@ -25,30 +25,14 @@ class Task {
 
     }
 
-    public function add() {
-        $data = Array (
-            "task_name" => "adm",
-            "task_text" => "John",
-
-        );
-
-        $d = ['task_name' => 'task1234', 'task_text' => 'task2 text']; 
-
-        //$this->db->where('id_task', 4);
-        //$task = $this->db->get($this->table_name);
-        //$newTask = new Task($this->db);
-
-        //$newTask->task_name = $task->task_name;
-        //$newTask->task_text = $task->task_text;
-        //$newTask->is_done = $task->is_done;
-
-        $id = $this->db->insert('task', $d);
-        var_dump($id);
-        //$this->db->where('id_task', 4);
-        //$this->db->update('task', $d);
-        if($id)
-        echo 'task was created. Id=' . $id;
-        else echo "error";
+    public function add($data) {
+        try {
+            $id = $this->db->insert('task', $data);
+            return (isset($id)) ? $id : null;    
+        }
+        catch (Exeption $ex) {
+            echo $ex->getMessage();
+       }
     }
 
     public function makeDone($id) {
